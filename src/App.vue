2a10b54fbb10970edecc70ed9075a20fff65ba6f
szw <357243507@qq.com>
2025-02-11 18:37:51 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld2 from './components/HelloWorld2.vue'
+</script>
+
+<template>
+ <div>
+  <HelloWorld2></HelloWorld2>
+  <HelloWorld msg="Vite + Vue" />
+ </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/HelloWorld2.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    {{ a }}
+  </div>
+  <button @click="add">add</button>
+</template>
+<script setup lang="ts">
+import { ref } from "vue";
+let a = ref(0);
+const add = () => {
+    a.value++
+};
+</script>
+<style lang="" scoped></style>
